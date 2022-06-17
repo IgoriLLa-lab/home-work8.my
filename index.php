a0d9aa7372cb40268c06a8e65d1e9afe72b0c7fc
Igor Kolesnik <crocusik@mail.ru>
2022-06-17 15:09:36 +0500

    доработанная домашняя работа #8

--- index.php
+++ index.php
@@ -1,23 +1,37 @@
-<?php
-?>
-
 <button type="button" id="get-text">Получить текст с сервера</button>
 <div id="result"></div>
 
 <script>
-    document.querySelector('#get-text').onclick = async () => {
+    document.querySelector('#get-text').onclick = async (key, value) => {
         try {
-            let response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
-            if (response.ok) {
-                let data = await response.text();
 
+            let url = 'https://www.cbr-xml-daily.ru/daily_json.js';
+            let request = await fetch(url);
 
+            if (request.ok) {
+                let data = await request.json()
 
-                // выведем данные в #result
-                document.querySelector('#result').innerHTML = data;
+                for (const value in data) {
+                    if (data[value]['USD']) {
+                        let usd = data[value]['USD'];
+                        let test = Object.entries(usd)
+
+                        test.forEach(([key, value]) => {
+                            let arr = (key, value);
+                            document.querySelector('#result').innerHTML = arr;
+
+                            document.cookie = arr;
+
+                            localStorage.setItem(usd, arr)
+                            sessionStorage.setItem(usd, arr)
+
+                        });
+                        alert(document.cookie);
+                    }
+                }
             }
         } catch (error) {
-            console.log(error);
+            alert(error);
         }
     }
 </script>
